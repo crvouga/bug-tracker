f490217a27803667107b9079c45cc79805935ce1
Christopher Vouga <crvouga@gmail.com>
2021-01-19 13:03:57 -0700

    removed template files

--- pages/api/people/index.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { people } from "../../../data";
-
-export default function handler(_1: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(people);
-}
