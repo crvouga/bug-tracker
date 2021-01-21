618eb4696aebe31a39028299009c94de483b48b6
Christopher Vouga <crvouga@gmail.com>
2021-01-20 18:42:51 -0700

    added logos

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,49 +2,22 @@ import NextAuth, { InitOptions } from "next-auth";
 import Providers from "next-auth/providers";
 import { NextApiRequest, NextApiResponse } from "next-auth/_utils";
 
+export const getEnvVariable = (key: string): string => {
+  const variable = process.env[key];
+
+  if (variable) {
+    return variable;
+  }
+
+  throw new Error(`${variable} is undefined`);
+};
+
 const options: InitOptions = {
   providers: [
-    Providers.Credentials({
-      id: "sign-in",
-      name: "PasswordCredentials",
-
-      credentials: {},
-
-      authorize: async (credentials) => {
-        const { emailAddress } = credentials;
-
-        console.log(credentials);
-        const user = {
-          emailAddress,
-        };
-
-        if (user) {
-          return Promise.resolve(user);
-        } else {
-          return Promise.resolve(null);
-        }
-      },
-    }),
-    Providers.Credentials({
-      id: "sign-up",
-      name: "PasswordCredentials",
-
-      credentials: {},
-
-      authorize: async (credentials) => {
-        const { emailAddress } = credentials;
-
-        console.log(credentials);
-        const user = {
-          emailAddress,
-        };
-
-        if (user) {
-          return Promise.resolve(user);
-        } else {
-          return Promise.resolve(null);
-        }
-      },
+    //dashboard: https://console.developers.google.com/apis/credentials/consent?project=bug-tracker-302401
+    Providers.Google({
+      clientId: getEnvVariable("GOOGLE_CLIENT_ID"),
+      clientSecret: getEnvVariable("GOOGLE_CLIENT_SECRET"),
     }),
   ],
 };
