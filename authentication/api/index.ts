2919947efa5837ab3ee9c45f1b46ca7117b155c1
Christopher Vouga <crvouga@gmail.com>
2021-01-20 18:45:46 -0700

    moved auth api  handler

---
+++ authentication/api/index.ts
@@ -0,0 +1,30 @@
+import NextAuth, { InitOptions } from "next-auth";
+import Providers from "next-auth/providers";
+import { NextApiRequest, NextApiResponse } from "next-auth/_utils";
+
+export const getEnvVariable = (key: string): string => {
+  const variable = process.env[key];
+
+  if (variable) {
+    return variable;
+  }
+
+  throw new Error(`${variable} is undefined`);
+};
+
+const options: InitOptions = {
+  providers: [
+    //dashboard: https://console.developers.google.com/apis/credentials/consent?project=bug-tracker-302401
+    Providers.Google({
+      clientId: getEnvVariable("GOOGLE_CLIENT_ID"),
+      clientSecret: getEnvVariable("GOOGLE_CLIENT_SECRET"),
+    }),
+  ],
+};
+
+export const handler = async (
+  request: NextApiRequest,
+  response: NextApiResponse
+) => {
+  return NextAuth(request, response, options);
+};
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,32 +1,3 @@
-import NextAuth, { InitOptions } from "next-auth";
-import Providers from "next-auth/providers";
-import { NextApiRequest, NextApiResponse } from "next-auth/_utils";
-
-export const getEnvVariable = (key: string): string => {
-  const variable = process.env[key];
-
-  if (variable) {
-    return variable;
-  }
-
-  throw new Error(`${variable} is undefined`);
-};
-
-const options: InitOptions = {
-  providers: [
-    //dashboard: https://console.developers.google.com/apis/credentials/consent?project=bug-tracker-302401
-    Providers.Google({
-      clientId: getEnvVariable("GOOGLE_CLIENT_ID"),
-      clientSecret: getEnvVariable("GOOGLE_CLIENT_SECRET"),
-    }),
-  ],
-};
-
-export const handler = async (
-  request: NextApiRequest,
-  response: NextApiResponse
-) => {
-  return NextAuth(request, response, options);
-};
+import { handler } from "../../../authentication/api";
 
 export default handler;
