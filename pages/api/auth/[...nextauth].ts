89d63ca83f0fcf717764beccfc6f015d61677142
Christopher Vouga <crvouga@gmail.com>
2021-01-26 01:25:08 -0700

    updated comments

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,21 +3,23 @@ import Providers from "next-auth/providers";
 import { NextApiRequest, NextApiResponse } from "next-auth/_utils";
 import { getEnvVariable } from "../../../utility/env";
 
-//source: https://github.com/nextauthjs/next-auth/issues/463
+//why?: https://github.com/nextauthjs/next-auth/issues/463
 const EMAIL_SERVER_HOST = "smtp.sendgrid.net";
 const EMAIL_SERVER_USERNAME = "apikey";
+//source: https://app.sendgrid.com/settings/api_keys
 const EMAIL_SERVER_PASSWORD = getEnvVariable("SEND_GRID_API_KEY");
 const EMAIL_SERVER = `smtp://${EMAIL_SERVER_USERNAME}:${EMAIL_SERVER_PASSWORD}@${EMAIL_SERVER_HOST}:587`;
+// source: https://app.sendgrid.com/settings/sender_auth/senders
 const EMAIL_FROM = getEnvVariable("EMAIL_FROM");
 
 const options: InitOptions = {
   providers: [
-    // console: https://console.developers.google.com/apis/credentials?pli=1&project=bug-tracker-302401&folder=&organizationId=
+    // source: https://console.developers.google.com/apis/credentials?pli=1&project=bug-tracker-302401&folder=&organizationId=
     Providers.Google({
       clientId: getEnvVariable("GOOGLE_CLIENT_ID"),
       clientSecret: getEnvVariable("GOOGLE_CLIENT_SECRET"),
     }),
-    // console: https://github.com/settings/apps/crvouga-bug-tracker
+    // source: https://github.com/settings/apps/crvouga-bug-tracker
     Providers.GitHub({
       clientId: getEnvVariable("GITHUB_CLIENT_ID"),
       clientSecret: getEnvVariable("GITHUB_CLIENT_SECRET"),
